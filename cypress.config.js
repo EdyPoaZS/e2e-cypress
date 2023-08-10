7b61b71c555dbf7c38724ef1233c2b2834bec430
Edenilson Machado <edenilsonmachado390@hotmail.com>
2023-08-10 17:37:43 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -7,9 +7,6 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
-    defaultCommandTimeout : 50000,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
+  projectId : 'ok8g58',
 })
